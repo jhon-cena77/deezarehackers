852ec911c6fb3f14a3e26336c2e83a7205916512
Kayden <128256865+jhon-cena77@users.noreply.github.com>
2024-02-19 13:34:14 -0700

    added varibles in js

---
+++ varibles.js
@@ -0,0 +1,24 @@
+//learn javascript
+
+//syntax:
+//; at end
+//very different from python
+
+//varibles
+//declare first
+//do not use anything you thing would be a function eg. var,let,document
+let hi;
+//then declare later
+hi = 8;
+//or all at once
+//number
+let myvar = 9;
+//boolean
+let mybool = true;//undefined,null,true,false
+//string
+let mystring = "hi";
+
+//use function.child(VARIBLE)
+//if you want not  varible then use quotes("")
+console.log("not stored data");
+console.log(mystring);
